5c3784dd74a8acc31aea4d244ecc8b788ac0e877
Elli M <elli@ttpk.fi>
2025-12-05 23:27:01 +0200

    moarrrrrrrr

--- system/appimage.nix
+++ system/appimage.nix
@@ -1,6 +1,10 @@
-programs.appimage = {
-  enable = true;
-  binfmt = true; # Enables direct execution (./appimage.AppImage)
-};
-# If FUSE is needed (many appimages use it):
-boot.kernelModules = [ "fuse" ]; # 
+{ config, lib, pkgs, ... }:
+
+{ 
+    programs.appimage = {
+        enable = true;
+        binfmt = true; # Enables direct execution (./appimage.AppImage)
+    };
+    # If FUSE is needed (many appimages use it):
+    boot.kernelModules = [ "fuse" ]; # 
+}
