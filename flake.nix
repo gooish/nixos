e52d5b5ccabbccd212d0eeb7f0ba1aaa50b17876
Elli M <elli@ttpk.fi>
2025-12-05 20:59:42 +0200

    Revert "nah thats stupid let's not"
    This reverts commit 7f21446d25d77fe89022ac300dcee6d406b84e94.

--- flake.nix
+++ flake.nix
@@ -4,10 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+	home-manager = {
+		url = "github:nix-community/home-manager/release-25.11";
+		inputs.nixpkgs.follows = "nixpkgs";
+	};
+	
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }:
