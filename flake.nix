b083459fa9007cf93b0092e8f967bd6dbbb8b178
Elli M <elli@ttpk.fi>
2025-12-05 21:01:27 +0200

    Revert to commit 7f21446d

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
-  description = "A very basic flake (with shadps4 overlay)";
+  description = "A very basic flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-
+	
 	home-manager = {
 		url = "github:nix-community/home-manager/release-25.11";
 		inputs.nixpkgs.follows = "nixpkgs";
@@ -11,44 +11,21 @@
 	
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }:
-    let
-      # import overlays from ./overlays/*.nix
-      overlays = [
-        (import ./overlays/shadps4.nix)
-      ];
-    in {
-      # Expose a package so you can `nix build .#shadps4`
-      packages.x86_64-linux = let
-        pkgs = import nixpkgs { system = "x86_64-linux"; overlays = overlays; };
-      in {
-        shadps4 = pkgs.shadps4;
-      };
+  outputs = inputs@{ nixpkgs, home-manager, ...} : {
+		nixosConfigurations =  {
+			nixpkgs.config.allowUnfree = true;
+			ten-of-swords = nixpkgs.lib.nixosSystem {
+				modules = [
+					./configuration.nix
 
-      # NixOS systems
-      nixosConfigurations = {
-        ten-of-swords = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
+					home-manager.nixosModules.home-manager {
+						home-manager.useGlobalPkgs = true;
+						home-manager.useUserPackages = true;
 
-          modules = [
-            ./configuration.nix
-
-            # home-manager module
-            home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.elli = import ./home.nix;
-            }
-
-            # attach overlays to the system's pkgs
-            ({ ... }: {
-              nixpkgs.overlays = overlays;
-            })
-          ];
-
-          # allow unfree (you had this before)
-          nixpkgs.config = { allowUnfree = true; };
-        };
-      };
-    };
+						home-manager.users.elli = import ./home.nix;
+					}
+				];
+			};
+    };  
+  };
 }
