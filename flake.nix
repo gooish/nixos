859955b8cbea7371b017d4d8ea1c275168a839c8
Elli M <elli@ttpk.fi>
2025-12-18 19:48:57 +0200

    it was still there

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
 
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, chaotic, ...} : {
+  outputs = inputs@{ nixpkgs, home-manager, ...} : {
 		nixosConfigurations =  {
 			nixpkgs.config.allowUnfree = true;
 			ten-of-swords = nixpkgs.lib.nixosSystem {
